b91de432e1d176d943ce9793621cd8077a1adf50
PrernaKumari12 <91779020+PrernaKumari12@users.noreply.github.com>
2021-10-02 14:13:19 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,26 @@
+package lecture_34;
+
+import java.util.Scanner;
+
+public class power {
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int p=scn.nextInt();
+		int ans=power(n,p);
+		System.out.println(ans);
+	}
+public static int power(int x,int n) {
+	if(n==0) {
+		return 1;
+	}
+	if(n%2==1) {
+	int sp=power(x, n/2);
+	
+	}
+	else {
+		int sp=power(x, n/2);
+		return sp*sp;
+	}
+}
+}
